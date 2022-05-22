0abed6fe81430a65562a76101e06d2fe9fcc6322
diecasti <diecasti@ucm.es>
2022-05-22 12:26:21 +0200

    rotacion hacia objetivo y solo se mueven hacia adelante

--- Assets/Scripts/Mirar.cs
+++ Assets/Scripts/Mirar.cs
@@ -8,33 +8,40 @@
    Autor: Federico Peinado 
    Contacto: email@federicopeinado.com
 */
+using UnityEngine;
+
 namespace UCM.IAV.Movimiento
 {
 
     /// <summary>
     /// Clase para modelar el comportamiento de SEGUIR a otro agente
     /// </summary>
-    public class Seguir : ComportamientoAgente
+    public class Mirar : ComportamientoAgente
     {
         /// <summary>
         /// Obtiene la dirección
         /// </summary>
         /// <returns></returns>
-        public override Direccion GetDireccion()
+        public float RotationSpeed = 1;
+        private Quaternion targetRotation;
+
+
+        public void doRotate()
         {
             // Si fuese un comportamiento de dirección dinámico en el que buscásemos alcanzar cierta velocidad en el agente, se tendría en cuenta la velocidad actual del agente y se aplicaría sólo la aceleración necesaria
             // Vector3 deltaV = targetVelocity - body.velocity;
             // Vector3 accel = deltaV / Time.deltaTime;
 
-            Direccion direccion = new Direccion();
-            direccion.lineal = /*objetivo.transform.position - */transform.forward;
-            direccion.lineal.Normalize();
-            direccion.lineal *= agente.aceleracionMax;
+
+            targetRotation = Quaternion.LookRotation(objetivo.transform.position - transform.position);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * RotationSpeed);
 
             // Podríamos meter una rotación automática en la dirección del movimiento, si quisiéramos
-            this.GetComponent<Mirar>().objetivo = objetivo;
-            this.GetComponent<Mirar>().doRotate();
-            return direccion;
+
+
+
+
+
         }
     }
 }
